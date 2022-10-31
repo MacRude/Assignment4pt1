1dc3a6dc73a5b13ef22929c27eb5c4d131df0afa
MacRude <78961065+MacRude@users.noreply.github.com>
2022-10-31 17:19:59 +0100

    Add files via upload

---
+++ ADO/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using Npgsql;
+
+var connectionString = "host=localhost;db=Northwind;uid=postgres;pwd=magnus1131";
+
+using var conn = new NpgsqlConnection(connectionString);
+conn.Open();
+
+using var cmd = new NpgsqlCommand();
+cmd.CommandText = "select * from categories";
+cmd.Connection = conn;
+
+var rdr = cmd.ExecuteReader();
+
+while (rdr.Read())
+{
+    Console.WriteLine($"Id={rdr.GetInt32(0)}, Name={rdr.GetString(1)}");
+}
